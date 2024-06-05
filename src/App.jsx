89e5e36f8a1b0b5d2e5d321f5992f4ce414cd7db
Gunjan2606 <101496845+Gunjan2606@users.noreply.github.com>
2024-06-05 19:26:16 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import navbar from './components/navbar/navbar'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hey</h1>
+      <navbar/>
+    </div>
+  )
+}
+
+export default App
